4028932e9f2bb872d9664384efdeeea2468125ba
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 21:25:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function createRock(x) {
   function moveRock() {
     if(checkCollision(rock)){
        endGame();
-     } else if(top < 400) {
+     } elseif(top < 400) {
          function down(){
            top += 2;
            rock.style.top = top + 'px';
@@ -78,7 +78,7 @@ function createRock(x) {
           }
           down();
         }
-      } else if(top === 400){
+      } elseif(top === 400){
         rock.remove();
       }
   }
