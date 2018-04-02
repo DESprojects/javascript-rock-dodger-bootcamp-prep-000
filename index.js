86024d5dd912fd9128f906c9c0bfe538ce8776b2
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 21:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const DODGER = document.getElementById('dodger')
 const GAME = document.getElementById('game')
 const GAME_HEIGHT = 400
 const GAME_WIDTH = 400
-const LEFT_ARROW = 37 
-const RIGHT_ARROW = 39 
+const LEFT_ARROW = 37
+const RIGHT_ARROW = 39
 const ROCKS = []
 const START = document.getElementById('start')
 
@@ -19,9 +19,6 @@ function checkCollision(rock) {
     const dodgerRightEdge = dodgerLeftEdge + 40;
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge + 20;
-
-        console.log(rockLeftEdge);
-        console.log(dodgerLeftEdge);
 
     if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
         (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
@@ -39,9 +36,9 @@ function createRock(x) {
   const rock = document.createElement('div')
   rock.className = 'rock'
   rock.style.left = `${x}px`
-  
+
   var top = 0
-  
+
   GAME.appendChild(rock);
 
   function moveRock() {
@@ -52,7 +49,7 @@ function createRock(x) {
            top += 2;
            rock.style.top = top + 'px';
            window.requestAnimationFrame(moveRock);
-          }
+      }
       else if(top >= 400){
         rock.remove();
       }
