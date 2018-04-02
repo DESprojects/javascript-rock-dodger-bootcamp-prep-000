660591d676c7fbd850f445bc8b0a729fc95bb2d7
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 21:49:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,9 @@ function endGame() {
   clearInterval(gameInterval);
   START.style.display = "block";
   window.removeEventListener('keydown', moveDodger);
-  document.querySelectorAll('.rock').remove();
+  ROCKS.forEach(function(rock){
+    rock.remove();
+  })
   ROCKS.splice(0);
   console.log(ROCKS)
   alert('YOU LOSE');
